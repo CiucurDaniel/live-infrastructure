551a45ea4ab8198775f6702cc982e5fd1acafbf3
Ciucur Daniel <ciucur.daniel14@gmail.com>
2022-06-14 10:10:19 +0300

    Add dev and prod config, improve readme

---
+++ prod/basic_app_server/variables.tf
@@ -0,0 +1,21 @@
+variable "ami_id" {
+  default = "ami-0a244485e2e4ffd03"
+  description = "The ami you want to use for the EC2 instance"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+  description = "The EC2 instance type you want to use: t2.micro, t2.medium"
+}
+
+variable "environment" {
+  description = "This variable should hold the environment currently being configured (dev, prod)"
+  default = "prod"
+}
+
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default = 8080
+}
